28e72a3c0325ffe6e40cf37e6307386eb2112c3b
dparnold <d.p.arnold@gmx.de>
2017-06-03 18:37:04 +0200

    added some functions to vocable class

--- vocable.cpp
+++ vocable.cpp
@@ -2,13 +2,45 @@
 #include <iostream>
 using namespace std;
 
-vocable::vocable(int id, string own_lan, string new_lan)
-: id(id), knowledge_value(0), own_language(own_lan), new_language(new_lan)
+//Constructor of a new vocable
+vocable::vocable(int id, string own_language, string new_language) 
+: id(id), knowledge_value(0),timesReviewed(0),reviewNumber(5), learnNumber(2), own_language(own_language), new_language(new_language)
 {
-cout << own_lan << " created"<<endl;
+//cout << "The vocable: "<<own_language<< own_language.size()<<" - "<< new_language <<new_language.size()<< " has been created"<<endl;
 }
 
 vocable::~vocable()
 {
     //dtor
 }
+
+void vocable::print()
+{
+	cout<<"ID: "<< id<<"\n"<<own_language<<"\n"<<new_language<< "\nKnowledge Value: "<< knowledge_value<< "\n\n\n";
+}
+
+void vocable::test(string typed)
+{	
+	string vocab = this->new_language;
+	bool correct = true;
+	for(unsigned int i = 0;i<max(vocab.size(),typed.size());i++)
+	{	
+		if (vocab[i] == typed[i])continue; // Check whether both strings are the same
+		else if ( (typed[i] == tolower(vocab[i])) || (typed[i]== toupper(vocab[i])) ) continue;	// If not: Ignore upper/lower case differences
+		else correct=false;
+	}
+	if (correct == true) this->knowledge_value+=1;
+	else if (correct == false) this->knowledge_value -=1;
+}
+
+string vocable::file_format()
+{
+	string line;
+	line += to_string(this->id)+"\t"; // for to_string you need to enable C++11 features
+	line += this->new_language+ "\t";
+	line += this->own_language+ "\t";
+	line += to_string(this->knowledge_value)+ "\n";
+	
+	return line;
+}
+
--- vocable.h
+++ vocable.h
@@ -1,20 +1,28 @@
 #include <string>
 #ifndef VOCABLE_H
 #define VOCABLE_H
-
+using namespace std;
 
 class vocable
 {
     public:
-        vocable(int id, std::string own_lan, std::string new_lan);
+        vocable(int id, std::string own_lan, std::string new_lan); // Constructor
         virtual ~vocable();
+		void print();
+		void test(std::string typed);
+		string file_format();
+		void review();
+		void learn();
+		
     protected:
     private:
         int id;
         int knowledge_value;
-        std::string own_language;
-        std::string new_language;
-        std::string mnemonic;
+        string own_language;
+        string new_language;
+		int reviewNumber;
+		int learnNumber;
+		int timesReviewed;
 
 };
 
